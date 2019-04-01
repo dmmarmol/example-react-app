81e962d8454f6087c881e3b4e43a7833a0b6e05c
Diego Martin Marmol <diegomartinmarmol@gmail.com>
2019-03-31 22:06:29 -0300

    Update Jest.config file

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-    testPathIgnorePatterns: ['/node_modules/'],
-    unmockedModulePathPatterns: ['./node_modules/react'],
+    // testPathIgnorePatterns: ['/node_modules/'],
+    // unmockedModulePathPatterns: ['./node_modules/react'],
     testRegex: '(/src/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+    // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+    // moduleDirectories: ['node_modules', 'bower_components', 'src'],
+    // rootDir: '<rootDir>/src',
+
+    transform: {
+        '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+        '^.+\\.(js|jsx)?$': 'babel-jest',
+        '^.+\\.(ts|tsx)?$': 'babel-jest',
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+    },
+    // testMatch: ['<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'],
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
