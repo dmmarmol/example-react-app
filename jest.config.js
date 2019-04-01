2e0b676cad16e1c679542dd285a1bb58f9068044
Diego Martin Marmol <diegomartinmarmol@gmail.com>
2019-03-31 22:28:07 -0300

    Upadte jest.config.file

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,38 @@
 module.exports = {
-    // testPathIgnorePatterns: ['/node_modules/'],
-    // unmockedModulePathPatterns: ['./node_modules/react'],
-    testRegex: '(/src/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-    // moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-    // moduleDirectories: ['node_modules', 'bower_components', 'src'],
-    // rootDir: '<rootDir>/src',
+    // Automatically clear mock calls and instances between every test
+    clearMocks: true,
 
-    transform: {
-        '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
-        '^.+\\.(js|jsx)?$': 'babel-jest',
-        '^.+\\.(ts|tsx)?$': 'babel-jest',
-    },
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    // testMatch: ['<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))'],
+    // An array of glob patterns indicating a set of files for which coverage information should be collected
+    collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
+
+    // The directory where Jest should output its coverage files
+    coverageDirectory: 'coverage',
+
+    // An array of file extensions your modules use
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+
+    // The paths to modules that run some code to configure or set up the testing environment before each test
+    // setupFiles: ['<rootDir>/enzyme.config.js'],
+
+    // The test environment that will be used for testing
+    testEnvironment: 'jsdom',
+
+    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+    testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+
+    // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
+    testURL: 'http://localhost',
+
+    // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     transformIgnorePatterns: ['<rootDir>/node_modules/'],
+
+    // Indicates whether each individual test should be reported during the run
+    verbose: false,
+    // The glob patterns Jest uses to detect test files
+    testMatch: [
+        '**/__tests__/**/*.js?(x)',
+        '**/?(*.)+(spec|test).js?(x)',
+        '**/__tests__/**/*.ts?(x)',
+        '**/?(*.)+(spec|test).ts?(x)',
+    ],
 };
